478e3076072c327f20421449de4c56bbd41edb33
Taline Araújo <taline.araujo@hotmail.com>
2024-02-27 16:57:19 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("rodando o projeto 1,2")
+
+// echo "# ExercicioFMT" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/TalineAraujo/ExercicioFMT.git
+// git push -u origin main
